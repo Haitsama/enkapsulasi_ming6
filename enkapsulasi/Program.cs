a18e1fb8285890f4a53a471dfe76e55a3713e017
Haitsama <haitsam@students.amikom.ac.id>
2023-04-16 15:15:53 +0700

    Add project files.

---
+++ enkapsulasi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enkapsulasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bank bank = new bank();
+
+            bank.Nama = "bank plecit";
+            bank.Alamat = "sentalu sariharjo";
+            bank.NO_akun = 34343432;
+            bank.Dompet = 100000;
+            Console.WriteLine("============================");
+            bank.cetak();
+            Console.WriteLine("============================");
+            Console.ReadLine();
+            bank.Transfer(50000);
+            bank.NO_REF = 545347970;
+            bank.cetak2();
+            Console.ReadLine();
+            bank.Deposit(50000);
+            bank.cetak();
+            Console.ReadLine();
+            bank.Withdraw(50000);
+            bank.cetak();
+            Console.ReadLine();
+            bank.Withdraw(150000);
+            bank.cetak();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ enkapsulasi/bank.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enkapsulasi
+{
+    internal class bank
+    {
+        //field
+        private string nama;
+        private string alamat;
+        private int nomor_akun ;
+        private double dompet;
+        private int no_ref;
+
+        public string Nama
+        //properties
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public string Alamat
+        {
+            get { return alamat; }
+            set { alamat = value; }
+        }
+
+        public int NO_akun
+        {
+            get { return nomor_akun; }
+            set { nomor_akun = value; }
+        }
+
+        public double Dompet
+        {
+            get { return dompet; }
+            set { dompet = value; }
+        }
+
+        public int NO_REF
+        {
+            get { return no_ref; }
+            set { no_ref = value; }
+        }
+
+        public void cetak()
+        {
+            Console.WriteLine("Nama: " + nama);
+            Console.WriteLine("Alamat: " + alamat);
+            Console.WriteLine("NO.Akun: " + nomor_akun);
+            Console.WriteLine("Dompet: " + dompet);
+        }
+
+        public void cetak2()
+        {
+            Console.WriteLine("Nama: " + nama);
+            Console.WriteLine("Alamat: " + alamat);
+            Console.WriteLine("NO.Akun: " + nomor_akun);
+            Console.WriteLine("NO.ref: " + no_ref);
+            Console.WriteLine("Dompet: " + dompet);
+            
+        }
+
+        public void Deposit(double jumlah)
+        {
+            dompet += jumlah;
+            Console.WriteLine("===================================================");
+            Console.WriteLine("Deposit sebesar Rp." + jumlah + " telah berhasil !!");
+            Console.WriteLine("===================================================");
+        }
+
+        public void Withdraw(double jumlah)
+        {
+            if (jumlah <= dompet)
+            {
+                dompet -= jumlah;
+                Console.WriteLine("===================================================");
+                Console.WriteLine("Withdraw sebesar Rp." + jumlah +" telah berhasil !!");
+                Console.WriteLine("===================================================");
+            }
+            else
+            {
+                Console.WriteLine("===================================================");
+                Console.WriteLine("Saldo tidak cukup!");
+                Console.WriteLine("===================================================");
+            }
+        }
+        public void Transfer(double jumlah)
+        {
+            if (jumlah <= dompet)
+            {
+                dompet -= jumlah;
+                Console.WriteLine("===================================================");
+                Console.WriteLine("Transfer sebesar Rp." + jumlah + " telah berhasil !!");
+                Console.WriteLine("===================================================");
+            }
+            else
+            {
+                Console.WriteLine("===================================================");
+                Console.WriteLine("Saldo tidak cukup!");
+                Console.WriteLine("===================================================");
+            }
+        }
+
+
+
+    }
+}
